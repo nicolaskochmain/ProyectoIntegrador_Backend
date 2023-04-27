b1cfa394382e6c6760c62ff94ad04b92a3eb4f4b
Nicolas Koch <119958241+nicolaskochmain@users.noreply.github.com>
2023-04-27 17:38:52 -0300

    main

--- src/main/java/com/portfolio/argentinaprograma/Controller/CPersona.java
+++ src/main/java/com/portfolio/argentinaprograma/Controller/CPersona.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,18 +48,18 @@ public class CPersona {
 //        return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
 //    }
 //    
-//    @PostMapping("/create")
-//    public ResponseEntity<?> create(@RequestBody DtoPersona dtoPersona) {
-//        if(StringUtils.isBlank(dtoPersona.getNombre())) {
-//            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-//        }
-//        if(sPersona.existsByNombre(dtoPersona.getNombre())) 
-//            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-//        
-//        Persona persona = new Persona(dtoPersona.getNombre(), dtoPersona.getDescripcion());
-//        sPersona.save(persona);
-//        return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
-//    }
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody DtoPersona dtoPersona) {
+        if(StringUtils.isBlank(dtoPersona.getNombre())) {
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(sPersona.existsByNombre(dtoPersona.getNombre())) 
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        
+        Persona persona = new Persona(dtoPersona.getNombre(), dtoPersona.getApellido(), dtoPersona.getImg(), dtoPersona.getDescripcion());
+        sPersona.save(persona);
+        return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtoPersona) {
