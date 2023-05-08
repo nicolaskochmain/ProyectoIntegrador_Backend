d964c57cb9b2146259569abe3478e57adb8784c4
Nicolas Koch <119958241+nicolaskochmain@users.noreply.github.com>
2023-05-08 10:18:42 -0300

    CORS fix

--- src/main/java/com/portfolio/argentinaprograma/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/argentinaprograma/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontendap-7f297.web.app", "http://localhost:4200/"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
